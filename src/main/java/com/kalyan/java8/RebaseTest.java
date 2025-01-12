559b66ed09d726c1c528bbb85dfb69d5cf774196
thalarikalyan <thalarikalyaninfo@gmail.com>
2025-01-11 23:10:28 -0500

    Updated the code changes in main branch

--- src/main/java/com/kalyan/java8/RebaseTest.java
+++ src/main/java/com/kalyan/java8/RebaseTest.java
@@ -5,7 +5,7 @@ public class RebaseTest {
 		System.out.println("RebaseTest.main()");
 		System.out.println("Rebase Main Test Method");
 		System.out.println("Added The changes");
-		System.out.println("I am Updated the Main Branch");
+		System.out.println("I am Updated the Main Branches");
 		System.out.println("I am updating code in feature branch");
 		System.out.println("I am updated the main branch After the Merge request is created ::");
 		System.out.println("=====================================================");
