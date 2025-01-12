c4f3525b9831ea998dfbc08b578baa61cc093cb4
thalarikalyan <thalarikalyaninfo@gmail.com>
2025-01-11 22:31:04 -0500

    Updated the code changes in RebaseTest

--- src/main/java/com/kalyan/java8/RebaseTest.java
+++ src/main/java/com/kalyan/java8/RebaseTest.java
@@ -4,6 +4,7 @@ public class RebaseTest {
 	public static void main(String[] args) {
 		System.out.println("RebaseTest.main()");
 		System.out.println("Rebase Main Test Method");
+		System.out.println("Added The changes");
 
 	}
 }
