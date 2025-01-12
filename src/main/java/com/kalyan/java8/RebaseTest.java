a59319d1b16dab48dc30d3e382f093ed3b3b553f
thalarikalyan <thalarikalyaninfo@gmail.com>
2025-01-11 22:26:12 -0500

    updated Rebase Test Class File

--- src/main/java/com/kalyan/java8/RebaseTest.java
+++ src/main/java/com/kalyan/java8/RebaseTest.java
@@ -3,6 +3,7 @@ package com.kalyan.java8;
 public class RebaseTest {
 	public static void main(String[] args) {
 		System.out.println("RebaseTest.main()");
+		System.out.println("Rebase Main Test Method");
 
 	}
 }
