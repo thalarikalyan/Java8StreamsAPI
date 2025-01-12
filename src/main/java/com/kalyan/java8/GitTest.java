cecf5eec72a77f3867310ae0b9f9ec7588899d51
thalarikalyan <thalarikalyaninfo@gmail.com>
2025-01-11 20:49:50 -0500

    Updated the Code changes

--- src/main/java/com/kalyan/java8/GitTest.java
+++ src/main/java/com/kalyan/java8/GitTest.java
@@ -4,13 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 
-
-
 		System.out.println("Welcome Thalari Kalyan");
-
-
-
-		
+		System.out.println("GitTest.main()");
 
 	}
 }
